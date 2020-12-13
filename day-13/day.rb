7d71a5178bf8fbcb751e1ff960afdfc9b49df18a
Mike Busch <mbusch@heroku.com>
2020-12-13 02:00:44 -0500

    part 2

--- day-13/day.rb
+++ day-13/day.rb
@@ -20,15 +20,15 @@ end
 
 def find_subsequent_time(time, buses)
   buses = buses.each_with_index.reject { |x, _| x.zero? }.sort_by(&:first).reverse
-  max = buses.first
-  time = max.first - max.last
-  while true
-    res = buses.all? do |(b, index)|
-      (time + index) % b == 0
+  (increment, rem), *buses = buses
+  time = (increment - rem) % increment
+  buses.each do |b, rem|
+    until (time + rem) % b == 0
+      time += increment
     end
-    return time if res
-    time += max.first
+    increment *= b
   end
+  return time
 end
 
 return unless $PROGRAM_NAME == __FILE__
