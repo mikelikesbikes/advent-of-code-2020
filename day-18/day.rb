c56ccb9f530a7fd73fd795ef111796a909926fc9
Mike Busch <mbusch@heroku.com>
2020-12-18 10:52:29 -0500

    some cleanup

--- day-18/day.rb
+++ day-18/day.rb
@@ -9,17 +9,19 @@ end
 
 def parse_input(input)
   input.split("\n").map do |line|
-    Expression.new(line.gsub(/\s+/, "").chars.map { |c| c.match?(/\d/) ? Integer(c) : c })
+    Expression.parse(line)
   end
 end
 
 ### CODE HERE ###
 
 Expression = Struct.new(:tokens) do
-  def evaluate(p)
-    i = 0
-    nums = []
-    ops = []
+  def self.parse(str)
+    new(str.gsub(/\s+/, "").chars.map { |c| c.match?(/\d/) ? Integer(c) : c })
+  end
+
+  def evaluate(p={ "+" => 1, "*" => 1, "(" => -1 })
+    nums, ops = [], []
     tokens.each do |token|
       case token
       when 0..9 then nums.push(token)
@@ -39,15 +41,12 @@ Expression = Struct.new(:tokens) do
   def process(nums, ops)
     a, b = nums.pop(2)
     op = ops.pop
-    case op
-    when "+" then nums.push(a + b)
-    when "*" then nums.push(a * b)
-    end
+    nums.push(a.send(op, b))
   end
 end
 
-def evaluate(input, p)
-  input.map { |exp| exp.evaluate(p) }
+def evaluate(input, *p)
+  input.map { |exp| exp.evaluate(*p) }
 end
 
 P1_PRECEDENCE = { "+" => 1, "*" => 1, "(" => -1 }
@@ -58,5 +57,5 @@ return unless $PROGRAM_NAME == __FILE__ || $PROGRAM_NAME.end_with?("ruby-memory-
 input = parse_input(read_input)
 
 ### RUN STUFF HERE ###
-puts evaluate(input, P1_PRECEDENCE).sum
+puts evaluate(input).sum
 puts evaluate(input, P2_PRECEDENCE).sum
