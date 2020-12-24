8d95b226d4f70506bd6cabbb56fea6542c6da095
Mike Busch <mbusch@heroku.com>
2020-12-24 08:41:34 -0500

    refactor tests

--- day-24/day_spec.rb
+++ day-24/day_spec.rb
@@ -31,36 +31,24 @@ describe "day" do
     parse_input(File.read("input.txt"))
   end
 
-  it "should ..." do
+  it "builds a tile set" do
     expect(tile_set.length).to eq 10
+  end
+
+  it "evolves the tile_set" do
+    [15, 12, 25, 14, 23, 28, 41, 37, 49, 37].each do |expected|
+      tile_set.evolve
+      expect(tile_set.length).to eq expected
+    end
+    90.times { tile_set.evolve }
+    expect(tile_set.length).to eq 2208
+  end
+
+  it "should pass part 1" do
     expect(actual_tile_set.length).to eq 500
   end
 
-  it "lives!" do
-    tile_set.evolve
-    expect(tile_set.length).to eq 15
-    tile_set.evolve
-    expect(tile_set.length).to eq 12
-    tile_set.evolve
-    expect(tile_set.length).to eq 25
-    tile_set.evolve
-    expect(tile_set.length).to eq 14
-    tile_set.evolve
-    expect(tile_set.length).to eq 23
-    tile_set.evolve
-    expect(tile_set.length).to eq 28
-    tile_set.evolve
-    expect(tile_set.length).to eq 41
-    tile_set.evolve
-    expect(tile_set.length).to eq 37
-    tile_set.evolve
-    expect(tile_set.length).to eq 49
-    tile_set.evolve
-    expect(tile_set.length).to eq 37
-
-    90.times { tile_set.evolve }
-    expect(tile_set.length).to eq 2208
-
+  it "should pass part 2" do
     100.times { actual_tile_set.evolve }
     expect(actual_tile_set.length).to eq 4280
   end
