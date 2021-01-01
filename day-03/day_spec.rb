208d43ad0346a857fc96da8cdb1041de33380a55
Mike Busch <mbusch@heroku.com>
2021-01-01 11:28:25 -0500

    add tests for actual input day 3

--- day-03/day_spec.rb
+++ day-03/day_spec.rb
@@ -18,11 +18,17 @@ describe "day" do
     INPUT
   end
 
+  let(:actual_input) do
+    parse_input(File.read("input.txt"))
+  end
+
   it "should return the tree count for a given slope" do
     expect(shred(input, 3, 1)).to eq 7
+    expect(shred(actual_input, 3, 1)).to eq 191
   end
 
   it "should calculate lots of slopes" do
     expect(multi_shred(input)).to eq 336
+    expect(multi_shred(actual_input)).to eq 1478615040
   end
 end
