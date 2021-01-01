c95c22191c516f51d768741095953091e46006c2
Mike Busch <mbusch@heroku.com>
2021-01-01 11:23:47 -0500

    actual input tests for day 1

--- day-01/day.rb
+++ day-01/day.rb
@@ -1,26 +1,20 @@
 require "set"
 
 def read_lines(filename = File.expand_path("input.txt", __dir__))
-  File.read(filename).split("\n").map(&:to_i)
+  File.read(filename)
+end
+
+def parse_input(input)
+  input.split("\n").map(&:to_i)
 end
 
 def find_tuple(input, size, target_sum)
-  if size == 2
-    return find_pair(input, target_sum)
-  end
-  input.permutation(size).find { |v| v.sum == target_sum }.reduce(:*)
-end
-
-def find_pair(input, target_sum)
-  nums = Set.new(input)
-  input.each do |v|
-    return v * (target_sum - v) if nums.include?(target_sum - v)
-  end
+  input.combination(size).find { |v| v.sum == target_sum }.reduce(:*)
 end
 
 return unless $PROGRAM_NAME == __FILE__
 
-input = read_lines
+input = parse_input(read_lines)
 
 puts find_tuple(input, 2, 2020)
 puts find_tuple(input, 3, 2020)
--- day-01/day_spec.rb
+++ day-01/day_spec.rb
@@ -3,7 +3,7 @@ require_relative "./day"
 
 describe "Day1" do
   let(:input) do
-    <<~INPUT.split("\n").map(&:to_i)
+    parse_input(<<~INPUT)
       1721
       979
       366
@@ -12,13 +12,18 @@ describe "Day1" do
       1456
     INPUT
   end
+
+  let(:actual_input) do
+    parse_input(File.read("input.txt"))
+  end
+
   it "should find a pair that sum to 2020" do
-    result = find_tuple(input, 2, 2020)
-    expect(result).to eq 514579
+    expect(find_tuple(input, 2, 2020)).to eq 514579
+    expect(find_tuple(actual_input, 2, 2020)).to eq 806656
   end
 
   it "should find a triplet that sums to 2020" do
-    result = find_tuple(input, 3, 2020)
-    expect(result).to eq 241861950
+    expect(find_tuple(input, 3, 2020)).to eq 241861950
+    expect(find_tuple(actual_input, 3, 2020)).to eq 230608320
   end
 end
